72c2141b86a8047815bf3f6ffdb8d7f597b05f41
Eeki <kalle.niukkanen@gmail.com>
2016-03-29 15:35:32 +0300

    webpack working with react and babel

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by kalle on 28/03/2016.
+ */
+
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
